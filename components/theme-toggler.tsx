f4fc4b95c1e7fd1559eba153ff9506271fbe7122
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-29 11:54:06 +0530

    Add theme toggler and provider for dark/light mode support

---
+++ components/theme-toggler.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { MoonStar, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export const ThemeToggler = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div className="border rounded-lg hover:bg-slate-500/30">
+      {theme === "dark" ? (
+        <SunIcon
+          className="w-6 h-6 cursor-pointer p-1"
+          onClick={() => setTheme("light")}
+        />
+      ) : (
+        <MoonStar
+          className="w-6 h-6 cursor-pointer p-1"
+          onClick={() => setTheme("dark")}
+        />
+      )}
+    </div>
+  );
+};
